def3a4f99262746ca90a39dda068a886a8c37dee
pinhead <mrmaxc2023@gmail.com>
2022-12-06 07:14:39 -0600

    changing how it runs

--- status.py
+++ status.py
@@ -1,60 +1,59 @@
 import rpc
 import time
+import signal
 import asyncio
+import warnings
 import windowUtils
 from time import mktime
 
-client_id = '1049193773982302240'  # Your application's client ID as a string. (This isn't a real client ID)
+warnings.filterwarnings("ignore", category=DeprecationWarning) # Prevents 3.10 from complaining about asyncio.ensure_future(activity())
+client_id = '1049193773982302240'  # Your application's client ID, this is what will display as your rpc's game
 rpc_obj = rpc.DiscordIpcClient.for_platform(client_id)  # Send the client ID to the rpc module
 print("RPC connection successful.")
 
 start_time = mktime(time.localtime())
-bigText = "I am currently"
-smallText = "testing my custom rpc"
-hardcodedGamesList = ["Deep Rock Galactic"]
 
 
 async def activity():
-    activity = dict(state=smallText, details=bigText, timestamps={
-        "start": start_time
-    }, assets={
-        "small_text": "image by nanbou",  # anything you like
-        "small_image": "pinhead_nanbou",  # must match the image key
-        "large_text": "image by pachikko",  # anything you like
-        "large_image": "pinhead_pachi"  # must match the image key
-    })
-    rpc_obj.set_activity(activity)
-    await asyncio.sleep(5)
+    while True:
+        detailsText, stateText = await windowCheck()
+        activity = dict(state=stateText, details=detailsText, timestamps={
+            "start": start_time
+        }, assets={
+            "small_text": "image by nanbou",  # anything you like
+            "small_image": "pinhead_nanbou",  # must match the image key
+            "large_text": "image by pachikko",  # anything you like
+            "large_image": "pinhead_pachi"  # must match the image key
+        })
+        rpc_obj.set_activity(activity)
+        await asyncio.sleep(5)
 
 
 async def windowCheck():
     fullscreenApp = windowUtils.findFullscreenApp()
+    window = windowUtils.getFocusedWindow()
+    print(fullscreenApp)
+    print(window)
     if fullscreenApp:
-        bigText = "Playing a Game"
-        smallText = fullscreenApp
+        detailsText = "Playing a Game"
+        stateText = fullscreenApp
+    elif window:
+        detailsText = "Playing a Game"
+        stateText = window
     else:
-        window = windowUtils.getFocusedWindow()
-        bigText = "Playing a Game"
-        smallText = window
-
-    # Messy way
-    # runningApps = windowUtils.getWindows()
-    # for window in runningApps:
-    #     if window in hardcodedGamesList:
-    #         bigText = "Playing a Game"
-    #         smallText = window
-    await asyncio.sleep(10)
-
-
-async def main():
-    await activity()
-    await windowCheck()
+        detailsText = "I am currently"
+        stateText = "using my custom rpc"
+    return detailsText, stateText
 
 
 if __name__ == "__main__":
     loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
+    asyncio.set_event_loop(loop) # Avoids errors in 3.11
+    asyncio.ensure_future(activity())
+
     try:
-        asyncio.run(main())
+        loop.run_forever()
     except KeyboardInterrupt:
         pass
+    finally:
+        loop.stop()
